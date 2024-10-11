d59da4dcd2e7db6e7f0374e9589218d3c9a1aa46
Alexey Gordiychenko <a.gordijchenko@gmail.com>
2024-10-12 00:07:47 +0500

    Add product's model and table

--- src/shopAPI/models.py
+++ src/shopAPI/models.py
@@ -1,5 +1,8 @@
+from typing import Any, Dict, Optional
+from uuid import UUID
 from pydantic import ConfigDict
 from sqlmodel import Field, SQLModel
+from shopAPI.database import IdMixin
 
 
 class ApiStatus(SQLModel):
@@ -23,3 +26,43 @@ class ApiStatus(SQLModel):
 
 class ResponseMessage(SQLModel):
     detail: str
+
+
+def field_example(param: Any) -> Dict[str, Dict[str, Any]]:
+    """
+    Returns field example for swagger documentation
+
+    It's a workaround for SQLModel bug,
+    see https://github.com/tiangolo/sqlmodel/discussions/833
+    """
+    return {"schema_extra": {"json_schema_extra": {"example": param}}}
+
+
+class ProductBase(SQLModel):
+    name: str = Field(nullable=False, **field_example("Product"))
+    description: str = Field(nullable=False, **field_example("A simple product"))
+    price: float = Field(nullable=False, **field_example(128.99))
+    amount: int = Field(nullable=False, **field_example(100))
+
+    model_config = ConfigDict(extra="forbid")
+
+
+class Product(IdMixin, ProductBase, table=True):
+    __tablename__ = "product"
+
+
+class ProductCreate(ProductBase):
+    pass
+
+
+class ProductUpdate(ProductBase):
+    name: Optional[str] = Field(None, **field_example("Updated product"))
+    description: Optional[str] = Field(
+        None, **field_example("A simple updated product")
+    )
+    price: Optional[float] = Field(None, **field_example(256.99))
+    amount: Optional[int] = Field(None, **field_example(200))
+
+
+class ProductResponse(ProductBase):
+    id: UUID
