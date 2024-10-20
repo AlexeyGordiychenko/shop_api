cafbe40602c36afd3b4b739f2054253da5665a60
Alexey Gordiychenko <a.gordijchenko@gmail.com>
2024-10-20 23:08:32 +0500

    Refactor: remove 'refresh' from Transactional class

--- src/shopAPI/database.py
+++ src/shopAPI/database.py
@@ -27,17 +27,12 @@ class IdMixin(SQLModel):
 
 
 class Transactional:
-    def __init__(self, refresh: bool = False):
-        self.refresh = refresh
-
     def __call__(self, function):
         @wraps(function)
         async def decorator(*args, **kwargs):
             try:
                 result = await function(*args, **kwargs)
                 await session.commit()
-                if self.refresh:
-                    await session.refresh(result)
                 return result
             except Exception as exception:
                 await session.rollback()
